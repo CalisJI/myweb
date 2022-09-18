96bc7ebb9f0e57a8491470fdf67e456818589f25
CalisJI <buinguyenquythi@gmail.com>
2022-09-19 00:34:27 +0700

    useref

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,22 +1,37 @@
-import React,{useState} from 'react'
+import React,{ forwardRef,useState, useImperativeHandle } from 'react'
 import { Link } from 'react-router-dom';
 import '../styles/SideBar.css';
-function SideBar(props) {
-   const [openwidth,setWidth] = useState("0px"); 
-   props.Opensidebar =()=>{
-        setWidth("250px");
+import logo from '../assets/calis_logo.png';
+const SideBar = forwardRef((props,ref) => {
+   const [openwidth,setWidth] = useState("55px"); 
+   const [logopos,setLogo] = useState("0px"); 
+    
+   useImperativeHandle(ref,()=>{
+        Opensidebar();
+   });
+
+   const Opensidebar =(event)=>{
+        if(openwidth === "55px"){
+            setWidth("250px");
+            setLogo("195px");
+        }
+        else{
+            setWidth("55px");
+            setLogo("0px");
+        }
    }
-   props.CloseSidebar = ()=>{
-    setWidth("0px");
-   }
+   
   return (
     <div className="sidebar" style={{width:openwidth}}>
-        <Link to='/' >Home</Link>
-        <Link to='/about' >About</Link>
-        <Link to='/contact' >Contact</Link>
-        <Link to='/project' >Project</Link>
+        {/* <img  src={logo} alt="calis_logo" className='logo' onClick={Opensidebar} style={{marginLeft:logopos}}/> */}
+        <div >
+            <Link className='Link' to='/' >Home</Link>
+            <Link className='Link' to='/about' >About</Link>
+            <Link className='Link' to='/contact' >Contact</Link>
+            <Link className='Link' to='/project' >Project</Link>
+        </div>
     </div>
   )
-}
+})
 
 export default SideBar
